aba62e03305ed629a09404372b13717928cf40fa
snowmerak <snowmerak@outlook.com>
2022-02-11 17:53:34 +0900

    remove some route

--- main.go
+++ main.go
@@ -7,30 +7,13 @@ import (
 	"time"
 
 	"github.com/snowmerak/compositor/proxy"
-	configRouter "github.com/snowmerak/compositor/router/config"
-	"github.com/snowmerak/compositor/router/register"
-	"github.com/snowmerak/lux"
 	"golang.org/x/net/http2"
 )
 
 func main() {
 	go func() {
-		app := lux.New(nil)
-
-		registerGroup := app.NewRouterGroup("/register")
-		registerGroup.GET("/:name", register.Get, nil)
-
-		configGroup := app.NewRouterGroup("/config")
-		configGroup.GET("/:name", configRouter.Get, nil)
-
-		if err := app.ListenAndServe2TLS("0.0.0.0:8080", "localhost/cert.pem", "localhost/key.pem"); err != nil {
-			panic(err)
-		}
-	}()
-
-	go func() {
 		server := &http.Server{
-			Addr:           "0.0.0.0:8080",
+			Addr:           "0.0.0.0:9999",
 			Handler:        http.HandlerFunc(proxy.Handler),
 			ReadTimeout:    10 * time.Second,
 			WriteTimeout:   10 * time.Second,
