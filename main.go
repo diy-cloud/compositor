6ea5548edae438252fb59b02013e5ef2edef28cb
snowmerak <snowmerak@outlook.com>
2022-02-15 19:19:24 +0900

    Update main.go

--- main.go
+++ main.go
@@ -1,36 +1,113 @@
 package main
 
 import (
+	"bufio"
+	"context"
 	"fmt"
-	"log"
+	"io"
+	"os"
+	"os/signal"
 
-	"github.com/snowmerak/compositor/container"
+	"github.com/docker/docker/api/types"
+	"github.com/docker/docker/api/types/container"
+	"github.com/docker/docker/api/types/network"
+	"github.com/docker/docker/client"
 )
 
 func main() {
-	end := container.End()
-
-	imageName, err := container.NewImageFromURL("docker.io/library/alpine:latest")
+	ctx := context.Background()
+	cli, err := client.NewClientWithOpts(client.FromEnv, client.WithAPIVersionNegotiation())
 	if err != nil {
-		log.Println(err)
+		panic(err)
 	}
-	fmt.Println(imageName)
 
-	if err := container.NewContainerBasedOnImage("container12", "snapshot12", imageName); err != nil {
-		log.Println(err)
-	}
-	fmt.Println("made container4")
-
-	exitStatus, err := container.ExecuteCommand("container12", "echo", "/", "/bin/sh", "-c", "echo HelloWorld!")
+	networkResp, err := cli.NetworkCreate(ctx, "test", types.NetworkCreate{
+		Driver:         "bridge",
+		CheckDuplicate: true,
+		Scope:          "local",
+		Internal:       true,
+	})
 	if err != nil {
-		log.Println(err)
-	}
-	fmt.Println("executed echo")
-
-	if exitStatus != nil {
-		status := <-exitStatus
-		fmt.Println(status.Result())
+		panic(err)
 	}
 
-	<-end
+	reader, err := cli.ImagePull(ctx, "docker.io/yeasy/simple-web", types.ImagePullOptions{})
+	if err != nil {
+		panic(err)
+	}
+	defer reader.Close()
+	io.Copy(os.Stdout, reader)
+
+	resp, err := cli.ContainerCreate(ctx, &container.Config{
+		Image: "yeasy/simple-web",
+		Tty:   false,
+	}, nil, &network.NetworkingConfig{
+		EndpointsConfig: map[string]*network.EndpointSettings{
+			"network": {
+				NetworkID: networkResp.ID,
+			},
+		},
+	}, nil, "")
+	if err != nil {
+		panic(err)
+	}
+
+	if err := cli.ContainerStart(ctx, resp.ID, types.ContainerStartOptions{}); err != nil {
+		panic(err)
+	}
+
+	statusCh, errCh := cli.ContainerWait(ctx, resp.ID, container.WaitConditionNotRunning)
+	select {
+	case err := <-errCh:
+		if err != nil {
+			panic(err)
+		}
+	case data := <-statusCh:
+		fmt.Println(data.StatusCode)
+	}
+
+	out, err := cli.ContainerLogs(ctx, resp.ID, types.ContainerLogsOptions{ShowStdout: true})
+	if err != nil {
+		panic(err)
+	}
+
+	go func() {
+		for {
+			scanner := bufio.NewReader(out)
+			line, _, err := scanner.ReadLine()
+			if err != nil {
+				if err == io.EOF {
+					break
+				}
+				panic(err)
+			}
+			fmt.Printf("[%s-%s]: %s\n", "alpine", resp.ID[:10], string(line[8:]))
+		}
+	}()
+
+	terminalChan := make(chan os.Signal, 1)
+	signal.Notify(terminalChan)
+	<-terminalChan
+
+	if err := cli.ContainerStop(ctx, resp.ID, nil); err != nil {
+		panic(err)
+	}
+
+	if err := cli.ContainerRemove(ctx, resp.ID, types.ContainerRemoveOptions{}); err != nil {
+		panic(err)
+	}
+
+	list, err := cli.ContainerList(ctx, types.ContainerListOptions{})
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Printf("container length: %v\n", len(list))
+	for _, container := range list {
+		fmt.Println(container.ID)
+	}
+
+	if err := cli.NetworkRemove(ctx, networkResp.ID); err != nil {
+		panic(err)
+	}
 }
