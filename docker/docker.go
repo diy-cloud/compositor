81cafde9dc92257c6f9fdc8cfd1af702b5476aa9
snowmerak <snowmerak@outlook.com>
2022-02-16 00:05:39 +0900

    create docker package

---
+++ docker/docker.go
@@ -0,0 +1,135 @@
+package docker
+
+import (
+	"context"
+	"fmt"
+	"io"
+	"os"
+	"runtime"
+	"strconv"
+	"sync"
+	"time"
+
+	"github.com/docker/docker/api/types"
+	"github.com/docker/docker/api/types/container"
+	"github.com/docker/docker/client"
+	"github.com/docker/go-connections/nat"
+)
+
+var ctx, cancel = context.WithCancel(context.Background())
+
+var containerList = []string{}
+
+func Close() error {
+	cli, err := client.NewClientWithOpts(client.FromEnv, client.WithAPIVersionNegotiation())
+	if err != nil {
+		return fmt.Errorf("ImportImageFromReader: %w", err)
+	}
+	cancel()
+	for _, c := range containerList {
+		if err := cli.ContainerStop(ctx, c, nil); err != nil {
+			return fmt.Errorf("ImportImageFromReader: %w", err)
+		}
+	}
+	return nil
+}
+
+func ImportImageFromReader(reader io.Reader, name string) error {
+	cli, err := client.NewClientWithOpts(client.FromEnv, client.WithAPIVersionNegotiation())
+	if err != nil {
+		return fmt.Errorf("ImportImageFromReader: %w", err)
+	}
+
+	ctx, cancel := context.WithTimeout(ctx, 60*time.Second)
+	defer cancel()
+	readCloser, err := cli.ImageImport(ctx, types.ImageImportSource{
+		Source:     reader,
+		SourceName: "-",
+	}, name, types.ImageImportOptions{})
+	if err != nil {
+		return fmt.Errorf("ImportImageFromReader: %w", err)
+	}
+	io.Copy(os.Stdout, readCloser)
+
+	return nil
+}
+
+var Ports = struct {
+	Map map[int]struct{}
+	sync.Mutex
+}{
+	Map: map[int]struct{}{},
+}
+
+const (
+	PortRangeStart = 49152
+	PortRangeEnd   = 65535
+)
+
+func CreateContainerByImage(image string, name string) (string, int, error) {
+	cli, err := client.NewClientWithOpts(client.FromEnv, client.WithAPIVersionNegotiation())
+	if err != nil {
+		return "", 0, fmt.Errorf("CreateContainerByImage: %w", err)
+	}
+
+	port := 0
+	Ports.Lock()
+	defer Ports.Unlock()
+	for i := PortRangeStart; i <= PortRangeEnd; i++ {
+		if _, ok := Ports.Map[i]; !ok {
+			port = i
+			Ports.Map[i] = struct{}{}
+			break
+		}
+	}
+
+	hostConfig := &container.HostConfig{
+		AutoRemove: true,
+		PortBindings: nat.PortMap{
+			"80/tcp": []nat.PortBinding{
+				{
+					HostIP:   "0.0.0.0",
+					HostPort: strconv.Itoa(port),
+				},
+			},
+		},
+	}
+
+	resp, err := cli.ContainerCreate(ctx, &container.Config{
+		Image: image,
+		ExposedPorts: nat.PortSet{
+			"80/tcp": struct{}{},
+		},
+		Tty: false,
+	}, hostConfig, nil, nil, name)
+	if err != nil {
+		return "", 0, fmt.Errorf("CreateContainerByImage: %w", err)
+	}
+
+	if err := cli.ContainerStart(ctx, resp.ID, types.ContainerStartOptions{}); err != nil {
+		return "", 0, fmt.Errorf("CreateContainerByImage: %w", err)
+	}
+
+	return resp.ID, port, nil
+}
+
+func RemoveContainer(id string, port int, count *int) error {
+	cli, err := client.NewClientWithOpts(client.FromEnv, client.WithAPIVersionNegotiation())
+	if err != nil {
+		return fmt.Errorf("RemoveContainer: %w", err)
+	}
+
+	for *count > 0 {
+		runtime.Gosched()
+	}
+
+	if err := cli.ContainerStop(ctx, id, nil); err != nil {
+		return fmt.Errorf("RemoveContainer: %w", err)
+	}
+
+	Ports.Lock()
+	defer Ports.Unlock()
+	delete(Ports.Map, port)
+
+	return nil
+}
