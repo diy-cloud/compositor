ce694866ff3fba9c2714ce3f5f2a950148745a63
snowmerak <snowmerak@outlook.com>
2022-02-16 23:20:27 +0900

    add http server and change ports

--- executor/exe.go
+++ executor/exe.go
@@ -22,14 +22,14 @@ func main() {
 		registerGroup := app.NewRouterGroup("/register")
 		registerGroup.POST("/:id", register.Post, nil)
 
-		if err := app.ListenAndServe2TLS(":9797", "localhost/cert.pem", "localhost/key.pem"); err != nil {
+		if err := app.ListenAndServe2TLS(":9999", "localhost/cert.pem", "localhost/key.pem"); err != nil {
 			panic(err)
 		}
 	}()
 
 	go func() {
 		server := &http.Server{
-			Addr:           "0.0.0.0:9999",
+			Addr:           "0.0.0.0:80",
 			Handler:        http.HandlerFunc(proxy.Handler),
 			ReadTimeout:    10 * time.Second,
 			WriteTimeout:   10 * time.Second,
@@ -38,7 +38,24 @@ func main() {
 
 		http2.ConfigureServer(server, nil)
 
-		fmt.Println("External Server is Listening on :9999")
+		fmt.Println("External HTTP Server is Listening on :80")
+		if err := server.ListenAndServe(); err != nil {
+			panic(err)
+		}
+	}()
+
+	go func() {
+		server := &http.Server{
+			Addr:           "0.0.0.0:443",
+			Handler:        http.HandlerFunc(proxy.Handler),
+			ReadTimeout:    10 * time.Second,
+			WriteTimeout:   10 * time.Second,
+			MaxHeaderBytes: 1 << 20,
+		}
+
+		http2.ConfigureServer(server, nil)
+
+		fmt.Println("External HTTPS Server is Listening on :443")
 		if err := server.ListenAndServeTLS("localhost/cert.pem", "localhost/key.pem"); err != nil {
 			panic(err)
 		}
